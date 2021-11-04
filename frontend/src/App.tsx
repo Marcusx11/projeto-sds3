813043bedcc235e88f691144b6e7ffe70b70286f
Marcusx11 <marcusx11@hotmail.com>
2021-11-04 20:48:50 -0300

    feat:
    começando a construir o componente de NavBar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,12 @@
 // Import do React não é mais necessário a partir da versão 17
+import NavBar from 'components/NavBar';
 import React from 'react';
 
 function App() {
   return (
     <div className="App">
       <h1 className="text-primary">Olá, mundo!</h1>
+      <NavBar />
     </div>
   );
 }
