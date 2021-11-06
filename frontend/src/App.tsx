479395b99727d291dc7783b8d97864b95d7c6eaa
Marcusx11 <marcusx11@hotmail.com>
2021-11-06 15:09:24 -0300

    feat:
    criação dos componentes de NavBar, Footer e DataTable

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,20 @@
 // Import do React não é mais necessário a partir da versão 17
-import NavBar from 'components/NavBar';
-import React from 'react';
+import DataTable from "components/DataTable";
+import Footer from "components/Footer";
+import NavBar from "components/NavBar";
+import React from "react";
 
 function App() {
   return (
-    <div className="App">
-      <h1 className="text-primary">Olá, mundo!</h1>
+    <>
       <NavBar />
-    </div>
+      <div className="container">
+        <h1 className="text-primary">Olá, mundo!</h1>
+
+        <DataTable />
+      </div>
+      <Footer />
+    </>
   );
 }
 
