5797e1f329585ff022c27999fa95137579d40dd2
Marcusx11 <marcusx11@hotmail.com>
2021-11-14 17:34:52 -0300

    feat:
    integração do BarChart com a api do backend

--- frontend/src/types/sale.ts
+++ frontend/src/types/sale.ts
@@ -1,6 +1,10 @@
-
-
 export type SaleSum = {
-    sellerName: string,
-    sum: number
-}
+  sellerName: string;
+  sum: number;
+};
+
+export type SaleSuccessRate = {
+  sellerName: string;
+  visited: number;
+  deals: number;
+};
