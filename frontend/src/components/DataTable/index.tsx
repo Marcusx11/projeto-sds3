fff3f255d4154bef60f542a9abc5d496288c9a77
Marcusx11 <marcusx11@hotmail.com>
2021-11-20 15:52:22 -0300

    feat:
    paginação do DataTable integrada com a api do backend

--- frontend/src/components/DataTable/index.tsx
+++ frontend/src/components/DataTable/index.tsx
@@ -1,10 +1,13 @@
 import axios from "axios";
+import Pagination from "components/Pagination";
 import { useEffect, useState } from "react";
 import { SalePage } from "types/sale";
 import { formatLocalDate } from "utils/format";
 import { BASE_URL } from "utils/requests";
 
 function DataTable() {
+  const [activePage, setActivePage] = useState(0);
+
   const [page, setPage] = useState<SalePage>({
     first: true,
     last: true,
@@ -13,17 +16,25 @@ function DataTable() {
     totalPages: 0,
   });
 
-  // Chamar apenas na primeira vez quando a página carregar
+  // Chamar apenas na primeira vez quando a página carregar (arrary vazio de observados)
+  // useEffect irá ser executado toda vez que o valor de activePage mudar
   useEffect(() => {
     axios
-      .get(`${BASE_URL}/sales/getAllPaginated?page=1&size=20&sort=date,desc`)
+      .get(
+        `${BASE_URL}/sales/getAllPaginated?page=${activePage}&size=20&sort=date,desc`
+      )
       .then((response) => {
         setPage(response.data);
       });
-  }, []);
+  }, [activePage]);
+
+  const changePage = (index: number) => {
+    setActivePage(index);
+  };
 
   return (
     <>
+      <Pagination page={page} onPageChange={changePage} />
       <div className="table-responsive">
         <table className="table table-striped table-sm">
           <thead>
