ff68481b4093fa6d78287a99de4ba86b871f498d
Marcusx11 <marcusx11@hotmail.com>
2021-11-20 16:00:22 -0300

    feat:
    setando variável de ambiente para o netlify

--- frontend/src/utils/requests.ts
+++ frontend/src/utils/requests.ts
@@ -1,1 +1,4 @@
-export const BASE_URL = "http://localhost:8080"
+/* "??" operador que verifica que se a variável de ambiente não estiver definida
+   no ambiente de aplicação em que ela estiver rodando, ele pega o valor que está
+   à direita */
+export const BASE_URL = process.env.REACT_APP_BACKEND_URL ?? "http://localhost:8080"
